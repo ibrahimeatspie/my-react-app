f61443c21a34c0295da9d1e4e0982474b32d1e09
ibrahimeatspie <104ibrahimshah@gmail.com>
2022-12-02 10:36:43 -0800

    can now save and show todos

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, {useState, useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
+  console.log(localStorage.getItem(LOCAL_STORAGE_KEY))
+
   const inputRef = useRef()
   const [todos, setTodos] = useState([])
 
-  function handleAddTodo(e) {
-    val = inputRef.current.value
-    if (val==""){
-      return
-    }else{
-      setTodos(prevTodos => {
-        return prevTodos
-      })
-      inputRef.current.value = null
+  useEffect(()=>{
+    //console.log(localStorage.getItem(LOCAL_STORAGE_KEY))
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    console.log(storedTodos)
+    setTodos(prevTodos=>{
+      return [...prevTodos, ...storedTodos]
+    })
+  }, [])
 
-    }
-    console.log(inputRef.current.value)
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function handleAddTodo(e) {
+    let name = inputRef.current.value
+    setTodos(function (previousTodos){
+      
+      return [...previousTodos, {name:name, id:uuidv4(), complete:false}]
+    })
+    inputRef.current.value = ''
+  }
+
+  function toggleTodo(id){
+
   }
 
   return (
